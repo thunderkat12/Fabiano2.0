cd5eea92227fcade9758b99a16380a4b2ee06816
Daniel Victor Rodrigues Aires Tavares <danielvictoraires9@gmail.com>
2026-02-15 10:42:36 -0300

    Preparando arquivos para deploy no Render

--- iniciar_api.bat
+++ iniciar_api.bat
@@ -1,4 +1,4 @@
 @echo off
-echo Iniciando API de Produtos...
-uvicorn api:app --reload --host 0.0.0.0 --port 8000
+echo Iniciando API de Produtos (Modo Local)...
+uvicorn api:app --reload
 pause
--- iniciar_ngrok.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Iniciando Ngrok na porta 8000...
-echo Se der erro, verifique se o ngrok esta instalado ou na mesma pasta.
-.\ngrok.exe http 8000
-pause
